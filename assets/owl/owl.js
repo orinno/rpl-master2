f9941bb29371332ca586f290ba5d2ec1bfe18c6a
oktavianirianti <oktavianirianti@gmail.com>
2022-04-27 12:40:11 +0700

    update layout galeri

---
+++ assets/owl/owl.js
@@ -0,0 +1,17 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  margin: 10,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
